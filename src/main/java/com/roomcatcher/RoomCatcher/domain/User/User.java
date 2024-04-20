a5acb60f00c7a5ffb3f0fbfc430537beb766019b
5hseok <ohhs1010@gmail.com>
2024-04-20 14:01:55 +0900

    :recycle: Refactor : User domain 객체 속성 추가 id, username, password, email 속성을 가지도록 코드 변경

--- src/main/java/com/roomcatcher/RoomCatcher/domain/User/User.java
+++ src/main/java/com/roomcatcher/RoomCatcher/domain/User/User.java
@@ -1,5 +1,21 @@
 package com.roomcatcher.RoomCatcher.domain.User;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class User {
+    @Id
+    private Long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
 
 }
